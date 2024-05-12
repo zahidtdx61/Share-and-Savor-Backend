ccc11eaaf562631a15652ae3830caedd52d5576c
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-05-12 14:04:28 +0600

    added cookie-parser

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const app = express();
+const cookieParser = require("cookie-parser");
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "https://share-and-savor.web.app/",
-    ],
-    credentials: true,
-  })
-);
+const corsOption = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://share-and-savor.web.app/",
+  ],
+  credentials: true,
+};
+
+app.use(cors(corsOption));
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send({
