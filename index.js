a17c652817b74d6cc6a0e037a64e1d0182d496fd
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-05-12 13:52:28 +0600

    added vercel config

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send({
+    message: "Server is running",
+  });
 });
 
 app.listen(3000, () => {
